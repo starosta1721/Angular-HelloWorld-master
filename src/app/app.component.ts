0f23901f029e3dbf7a06a82aefa6e8ad09bdcd27
starosta1721 <starosta1721@gmail.com>
2019-08-06 11:28:57 +0300

    Try to add servise file but still not working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import sampleJSON from './sampleJSON.json';
+
 import {CreateArrayForDisplay} from './data.service';
 
 @Component({
@@ -18,54 +18,26 @@ import {CreateArrayForDisplay} from './data.service';
     <img src="{{i}}" alt = "logo">
  
   </div>
-  `
+  `,
+  providers: [CreateArrayForDisplay]
 })
 export class AppComponent {
-  number: number;
-  questionsArray: string[] = [...sampleJSON];
-  
-  uniqueQuestion: any[] = []; 
-  arrayStrings: string[] = [];
-  arrayUrl: string[] = [];
-  getRandomNumber() {
-    let randomNumber: number = Math.floor((Math.random()*this.questionsArray.length));
-    return randomNumber;
-  };
-  
-  getQuestion(number: number) {
-    let elementsInArray: number = this.questionsArray.length - 1;
-    let arrayOfRandomNumber: number[] = [];
-    //create array with unique numbers of questions
-    while (arrayOfRandomNumber.length != elementsInArray) {
-      let tempNumber: number;
-      tempNumber = this.getRandomNumber();
-      if (!arrayOfRandomNumber.includes(tempNumber)) {
-        arrayOfRandomNumber.push(tempNumber);
-      } else {tempNumber = this.getRandomNumber()};
-      arrayOfRandomNumber.sort();
-      if (arrayOfRandomNumber.length == number) {
-        break;
-      };
-    };
-    //.log(arrayOfRandomNumber);
-    //array with unique questions
-    this.uniqueQuestion = arrayOfRandomNumber.map((item: any) => { 
-      return(this.questionsArray[item]);
-    });
-    //console.log(this.uniqueQuestion);
+  arrayStrings: [] = [];
+  arrayUrl: [] = [];
+  createArrayForDisplay: any;
+  constructor ( createArrayForDisplay: CreateArrayForDisplay) {    
     
-    for (let i = 0; i < this.uniqueQuestion.length; i++) {
-      //if (!(/http:/i.test(this.uniqueQuestion[i])) && !(/https:/i.test(this.uniqueQuestion[i])))
-      if (!(this.uniqueQuestion[i].includes("http"))) 
-      {
-        this.arrayStrings.push(this.uniqueQuestion[i]);
-      } else {
-        this.arrayUrl.push(this.uniqueQuestion[i]);
-        };
-    };
-    //console.log(this.questionsArray);    
-    // console.log(this.arrayStrings);
-        // console.log(this.arrayUrl);
+  };
+  getQuestion(number: number) {
+    this.createArrayForDisplay.getQuestion(number);
+  }
+  
+  ngOnInit(){
+    
+    this.arrayStrings = this.createArrayForDisplay.getArrayStrings(); 
+    this.arrayUrl = this.createArrayForDisplay.getArrayUrl(); 
   };
 };
 
+// Backendless JS-SDK
+//https://www.educba.com/typescript-type-vs-interface/
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,13 +1,58 @@
+import sampleJSON from './sampleJSON.json';
+
 export class CreateArrayForDisplay {
-  
-    private data: string[] = [ "Apple iPhone XR", "Samsung Galaxy S9",  "Nokia 9"];
+    number: number;
+    questionsArray: string[] = [...sampleJSON];
+    
+    uniqueQuestion: any[] = []; 
+    arrayStrings: string[] = [];
+    arrayUrl: string[] = [];
+    getRandomNumber() {
+      let randomNumber: number = Math.floor((Math.random()*this.questionsArray.length));
+      return randomNumber;
+    };
+    
+    getQuestion(number: number) {
+      let elementsInArray: number = this.questionsArray.length - 1;
+      let arrayOfRandomNumber: number[] = [];
+      //create array with unique numbers of questions
+      while (arrayOfRandomNumber.length != elementsInArray) {
+        let tempNumber: number;
+        tempNumber = this.getRandomNumber();
+        if (!arrayOfRandomNumber.includes(tempNumber)) {
+          arrayOfRandomNumber.push(tempNumber);
+        } else {tempNumber = this.getRandomNumber()};
+        arrayOfRandomNumber.sort();
+        if (arrayOfRandomNumber.length == number) {
+          break;
+        };
+      };
+      //.log(arrayOfRandomNumber);
+      //array with unique questions
+      this.uniqueQuestion = arrayOfRandomNumber.map((item: any) => { 
+        return(this.questionsArray[item]);
+      });
+      //console.log(this.uniqueQuestion);
       
-    getData(): string[] {
-          
-        return this.data;
-    }
-    addData(name: string){
-          
-        this.data.push(name);
-    }
-}
+      for (let i = 0; i < this.uniqueQuestion.length; i++) {
+        //if (!(/http:/i.test(this.uniqueQuestion[i])) && !(/https:/i.test(this.uniqueQuestion[i])))
+        if (!(this.uniqueQuestion[i].includes("http"))) 
+        {
+          this.arrayStrings.push(this.uniqueQuestion[i]);
+        } else {
+          this.arrayUrl.push(this.uniqueQuestion[i]);
+          };
+         
+      };
+      //console.log(this.questionsArray);    
+      // console.log(this.arrayStrings);
+        // console.log(this.arrayUrl);
+
+    };
+getArrayStrings() {
+    return this.arrayStrings;
+};
+getArrayUrl() {
+    return this.arrayUrl;
+}    
+};
